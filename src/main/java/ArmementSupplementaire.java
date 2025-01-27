465e85252b580ae79676c57e362069e85fc6ad4c
22302683 <moh78078000@gmail.com>
2025-01-27 12:04:22 +0100

    "Ajout des commentaires pour la classe ArmementSupplementaire"

--- src/main/java/ArmementSupplementaire.java
+++ src/main/java/ArmementSupplementaire.java
@@ -1,14 +1,34 @@
-public abstract class  ArmementSupplementaire extends Voilier   {
+/**
+ * La classe abstraite ArmementSupplementaire étend Voilier.
+ * Cette classe représente un armement supplémentaire pour un voilier.
+ */
+public abstract class ArmementSupplementaire extends Voilier {
+
+    /**
+     * Le composant du voilier à décorer.
+     */
     Voilier composantadecore;
 
+    /**
+     * Constructeur de la classe ArmementSupplementaire.
+     * @param composantadecore Le composant du voilier à décorer.
+     */
     public ArmementSupplementaire(Voilier composantadecore) {
         super();
         this.composantadecore = composantadecore;
-
     }
-    public void appliqueSuivreRoute(){
+
+    /**
+     * Applique la méthode suivre route du composant décoré.
+     */
+    public void appliqueSuivreRoute() {
         composantadecore.appliquerSuivreRoute();
     }
+
+    /**
+     * Définit la méthode suivre route du composant décoré.
+     * @param suivreRoute L'objet SuivreRoute à définir.
+     */
     public void setSuivreRoute(SuivreRoute suivreRoute) {
         composantadecore.setSuivreRoute(suivreRoute);
     }
