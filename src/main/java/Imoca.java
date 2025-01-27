66d4d2f1aa7b48922ed178ef37e62f5a9f5f91e8
22302683 <moh78078000@gmail.com>
2025-01-27 12:09:11 +0100

    "Ajout des commentaires pour la classe Imoca"

--- src/main/java/Imoca.java
+++ src/main/java/Imoca.java
@@ -1,4 +1,14 @@
-public class Imoca extends Monocoque{
+/**
+ * La classe Imoca étend Monocoque.
+ * Cette classe représente un voilier de classe Imoca.
+ */
+public class Imoca extends Monocoque {
+
+    /**
+     * Constructeur de la classe Imoca.
+     * @param nom Le nom du voilier.
+     * @param skipper Le skipper du voilier.
+     */
     public Imoca(String nom, String skipper) {
         super(nom, skipper);
     }
