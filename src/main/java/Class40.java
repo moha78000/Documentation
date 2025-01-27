6a35885bf5a10404eb9e799c5f0ec966c6ef4d45
22302683 <moh78078000@gmail.com>
2025-01-27 12:06:27 +0100

    "Ajout des commentaires pour la classe Class40"

--- src/main/java/Class40.java
+++ src/main/java/Class40.java
@@ -1,12 +1,24 @@
-public class Class40 extends Monocoque{
+/**
+ * La classe Class40 étend Monocoque.
+ * Cette classe représente un voilier de classe 40.
+ */
+public class Class40 extends Monocoque {
 
+    /**
+     * Constructeur de la classe Class40.
+     * @param nom Le nom du voilier.
+     * @param skipper Le skipper du voilier.
+     */
     public Class40(String nom, String skipper) {
-        super(nom , skipper);
-
-
-
+        super(nom, skipper);
     }
-    public String toString(){
+
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de l'objet Class40.
+     * @return Une chaîne de caractères représentant l'objet Class40.
+     */
+    @Override
+    public String toString() {
         return "Class40" + super.toString();
     }
 }
