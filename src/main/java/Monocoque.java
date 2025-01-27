996fd239816076de519de581c721813f54be1779
22302683 <moh78078000@gmail.com>
2025-01-27 12:18:24 +0100

    "Ajout des commentaires pour la classe Monocoque"

--- src/main/java/Monocoque.java
+++ src/main/java/Monocoque.java
@@ -1,18 +1,34 @@
+/**
+ * La classe Monocoque étend Voilier.
+ * Cette classe représente un voilier de type monocoque.
+ */
 public class Monocoque extends Voilier {
 
-    public Monocoque(String nom , SuivreRoute suivreroute , String skipper) {
-
-        super(nom , suivreroute);
-
+    /**
+     * Constructeur de la classe Monocoque avec SuivreRoute.
+     * @param nom Le nom du voilier.
+     * @param suivreroute L'objet SuivreRoute.
+     * @param skipper Le skipper du voilier.
+     */
+    public Monocoque(String nom, SuivreRoute suivreroute, String skipper) {
+        super(nom, suivreroute);
     }
-    public Monocoque(String nom , String skipper) {
-        super(nom , skipper);
+
+    /**
+     * Constructeur de la classe Monocoque sans SuivreRoute.
+     * @param nom Le nom du voilier.
+     * @param skipper Le skipper du voilier.
+     */
+    public Monocoque(String nom, String skipper) {
+        super(nom, skipper);
     }
 
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de l'objet Monocoque.
+     * @return Une chaîne de caractères représentant l'objet Monocoque.
+     */
     @Override
     public String toString() {
-        return " Le nom est : " + nom + " Le bateau est de type : Monocoque ";
+        return "Le nom est : " + nom + " Le bateau est de type : Monocoque ";
     }
-
-
 }
