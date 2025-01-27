ed55716b46d7064d491a6211ed1469374cad6f5e
22302683 <moh78078000@gmail.com>
2025-01-27 12:16:07 +0100

    "Ajout des commentaires pour la classe InscriptionMulticoquecoque"

--- src/main/java/InscriptionMulticoque.java
+++ src/main/java/InscriptionMulticoque.java
@@ -1,24 +1,35 @@
-public class InscriptionMulticoque extends InscriptionVoilier{
+/**
+ * La classe InscriptionMulticoque étend InscriptionVoilier.
+ * Cette classe représente une inscription spécifique pour les multicoques.
+ */
+public class InscriptionMulticoque extends InscriptionVoilier {
 
-
-    public InscriptionMulticoque(){
+    /**
+     * Constructeur de la classe InscriptionMulticoque.
+     */
+    public InscriptionMulticoque() {
         super();
     }
 
-    public Voilier factoryMethod(String nom , String skipper , String compare) {
-        if (compare == "Multi50"){
-            return new Multi50(nom , skipper);
-
+    /**
+     * Méthode factory pour créer un voilier en fonction de son type.
+     * @param nom Le nom du voilier.
+     * @param skipper Le skipper du voilier.
+     * @param compare Le type de voilier (par exemple, "Multi50", "Ultim", "RhumMulti").
+     * @return Un objet Voilier correspondant au type spécifié, ou null si le type n'est pas reconnu.
+     */
+    public Voilier factoryMethod(String nom, String skipper, String compare) {
+        if (compare.equals("Multi50")) {
+            return new Multi50(nom, skipper);
         }
 
-        if (compare == "Ultim"){
-            return new Ultim(nom , skipper);
-        }
-        if (compare == "RhumMulti"){
-            return new RhumMulti(nom , skipper);
-
+        if (compare.equals("Ultim")) {
+            return new Ultim(nom, skipper);
         }
 
+        if (compare.equals("RhumMulti")) {
+            return new RhumMulti(nom, skipper);
+        }
 
         return null;
     }
