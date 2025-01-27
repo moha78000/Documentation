124ce88ed97796394f7a517a9d7c975e834d1898
22302683 <moh78078000@gmail.com>
2025-01-27 12:14:44 +0100

    "Ajout des commentaires pour la classe InscriptionMonocoque"

--- src/main/java/InscriptionMonocoque.java
+++ src/main/java/InscriptionMonocoque.java
@@ -1,25 +1,37 @@
 import java.util.List;
 
-public class InscriptionMonocoque extends InscriptionVoilier{
+/**
+ * La classe InscriptionMonocoque étend InscriptionVoilier.
+ * Cette classe représente une inscription spécifique pour les monocoques.
+ */
+public class InscriptionMonocoque extends InscriptionVoilier {
 
-    public InscriptionMonocoque(){
+    /**
+     * Constructeur de la classe InscriptionMonocoque.
+     */
+    public InscriptionMonocoque() {
         super();
     }
 
-    public Voilier factoryMethod(String nom , String skipper , String compare) {
-        if (compare == "Class40"){
-            return new Class40(nom , skipper);
-
+    /**
+     * Méthode factory pour créer un voilier en fonction de son type.
+     * @param nom Le nom du voilier.
+     * @param skipper Le skipper du voilier.
+     * @param compare Le type de voilier (par exemple, "Class40", "Imoca", "RhumMono").
+     * @return Un objet Voilier correspondant au type spécifié, ou null si le type n'est pas reconnu.
+     */
+    public Voilier factoryMethod(String nom, String skipper, String compare) {
+        if (compare.equals("Class40")) {
+            return new Class40(nom, skipper);
         }
 
-        if (compare == "Imoca"){
-            return new Imoca(nom , skipper);
-        }
-        if (compare == "RhumMono"){
-            return new RhumMono(nom , skipper);
-
+        if (compare.equals("Imoca")) {
+            return new Imoca(nom, skipper);
         }
 
+        if (compare.equals("RhumMono")) {
+            return new RhumMono(nom, skipper);
+        }
 
         return null;
     }
