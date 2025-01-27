6b09fb0f3056432b6c109bb00aa8e1a09827a12f
22302683 <moh78078000@gmail.com>
2025-01-27 12:07:51 +0100

    "Ajout des commentaires pour la classe Foil"

--- src/main/java/Foil.java
+++ src/main/java/Foil.java
@@ -1,10 +1,23 @@
+/**
+ * La classe Foil étend ArmementSupplementaire.
+ * Cette classe représente un armement foil pour un voilier.
+ */
 public class Foil extends ArmementSupplementaire {
 
-    public Foil(Voilier composantadecore){
+    /**
+     * Constructeur de la classe Foil.
+     * @param composantadecore Le composant du voilier à décorer.
+     */
+    public Foil(Voilier composantadecore) {
         super(composantadecore);
     }
 
-    public String toString(){
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de l'objet Foil.
+     * @return Une chaîne de caractères représentant l'objet Foil.
+     */
+    @Override
+    public String toString() {
         return composantadecore.toString() + " Armement foil ";
     }
 }
