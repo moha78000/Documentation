bad3e1406b6aab7f7732ffb5796fc941891f8546
22302683 <moh78078000@gmail.com>
2025-01-27 12:17:44 +0100

    "Ajout des commentaires pour la classe InscriptionVoilier"

--- src/main/java/InscriptionVoilier.java
+++ src/main/java/InscriptionVoilier.java
@@ -1,18 +1,39 @@
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * La classe abstraite InscriptionVoilier représente une inscription générique pour les voiliers.
+ * Elle permet d'enregistrer et de créer des voiliers.
+ */
 public abstract class InscriptionVoilier {
-   static List<Voilier> voiliers;
 
+    /**
+     * Liste statique des voiliers enregistrés.
+     */
+    static List<Voilier> voiliers;
+
+    /**
+     * Constructeur de la classe InscriptionVoilier.
+     * Initialise la liste des voiliers.
+     */
     public InscriptionVoilier() {
         voiliers = new LinkedList<>();
-
     }
+
+    /**
+     * Méthode pour enregistrer un voilier dans la liste.
+     * @param voilier Le voilier à enregistrer.
+     */
     public void enregistrerVoilier(Voilier voilier) {
         voiliers.add(voilier);
     }
 
-    public abstract Voilier factoryMethod(String nom , String skipper , String categorie );
-
-
+    /**
+     * Méthode factory abstraite pour créer un voilier en fonction de son nom, skipper et catégorie.
+     * @param nom Le nom du voilier.
+     * @param skipper Le skipper du voilier.
+     * @param categorie La catégorie du voilier.
+     * @return Un objet Voilier correspondant aux paramètres spécifiés.
+     */
+    public abstract Voilier factoryMethod(String nom, String skipper, String categorie);
 }
