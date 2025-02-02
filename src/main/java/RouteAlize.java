4a693688fc922cb4db0d764e6412ad2cc24602e2
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-02 15:05:23 +0100

    "Ajout de commentaires pour la classe RouteAlize.java

--- src/main/java/RouteAlize.java
+++ src/main/java/RouteAlize.java
@@ -1,5 +1,14 @@
+/**
+ * La classe RouteAlize représente une route maritime empruntant les alizés.
+ * Elle implémente l'interface SuivreRoute et définit la méthode suivreRoute
+ * pour afficher un message indiquant que cette route est suivie.
+ */
 public class RouteAlize implements SuivreRoute {
+
+    /**
+     * Suit la route des alizés en affichant un message.
+     */
     public void suivreRoute() {
-        System.out.println("Vous emprunté la route des alizés");
+        System.out.println("Vous empruntez la route des alizés");
     }
 }
