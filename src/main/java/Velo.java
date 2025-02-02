0b10c02f364eacfa65fd0c4c33bfd2eefa3eeaa4
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-02 15:06:56 +0100

    "Ajout de commentaires pour la classe Velo.java

--- src/main/java/Velo.java
+++ src/main/java/Velo.java
@@ -1,12 +1,25 @@
-public class Velo extends ArmementSupplementaire{
+/**
+ * La classe Velo représente un armement supplémentaire pour un voilier.
+ * Elle hérite de la classe ArmementSupplementaire et permet d'ajouter un vélo
+ * comme équipement supplémentaire à un voilier.
+ */
+public class Velo extends ArmementSupplementaire {
 
-
-
-    public Velo(Voilier composantadecore){
+    /**
+     * Constructeur qui ajoute un vélo à un voilier en tant qu'armement supplémentaire.
+     *
+     * @param composantadecore Le voilier auquel on ajoute le vélo comme armement.
+     */
+    public Velo(Voilier composantadecore) {
         super(composantadecore);
     }
-    public String toString(){
+
+    /**
+     * Retourne une représentation textuelle du voilier avec l'ajout de l'armement vélo.
+     *
+     * @return Une chaîne de caractères contenant les informations du voilier et l'armement "vélo".
+     */
+    public String toString() {
         return composantadecore.toString() + " Armement vélo ";
     }
-
 }
