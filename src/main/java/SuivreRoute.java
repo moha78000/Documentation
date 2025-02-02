e555db60b7857e6236439de0827b2cc8efbda6a7
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-02 15:06:04 +0100

    "Ajout de commentaires pour l'interface SuivreRoute.java

--- src/main/java/SuivreRoute.java
+++ src/main/java/SuivreRoute.java
@@ -1,4 +1,13 @@
+/**
+ * L'interface SuivreRoute définit un contrat pour les différentes routes maritimes.
+ * Toute classe qui implémente cette interface doit fournir une implémentation
+ * de la méthode suivreRoute, qui permet d'indiquer la route suivie.
+ */
 public interface SuivreRoute {
-    public void suivreRoute();
 
+    /**
+     * Définit le comportement de suivi d'une route maritime.
+     * Chaque classe implémentant cette méthode doit spécifier comment suivre la route.
+     */
+    public void suivreRoute();
 }
