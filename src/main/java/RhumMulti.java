1f7eb57dbb77652220f9f80c0a4374abe41c5d6e
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-02 15:05:01 +0100

    "Ajout de commentaires pour la classe RhumMulti.java

--- src/main/java/RhumMulti.java
+++ src/main/java/RhumMulti.java
@@ -1,8 +1,26 @@
-public class RhumMulti extends Multicoque{
+/**
+ * La classe RhumMulti représente un type spécifique de multicoque.
+ * Elle hérite de la classe Multicoque et permet de créer un bateau
+ * participant à la catégorie Rhum Multi avec un nom et un skipper.
+ */
+public class RhumMulti extends Multicoque {
 
+    /**
+     * Constructeur qui crée un bateau RhumMulti avec un nom et un skipper.
+     *
+     * @param nom     Le nom du bateau.
+     * @param skipper Le nom du skipper.
+     */
     public RhumMulti(String nom, String skipper) {
         super(nom, skipper);
     }
+
+    /**
+     * Retourne une représentation textuelle de l'objet RhumMulti.
+     *
+     * @return Une chaîne de caractères contenant "RhumMulti" suivi des informations du multicoque.
+     */
+    @Override
     public String toString() {
         return "RhumMulti" + super.toString();
     }
