2d66ec6839066292052d5e52647e0be34ba6422f
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-02 15:04:47 +0100

    "Ajout de commentaires pour la classe RhumMono.java

--- src/main/java/RhumMono.java
+++ src/main/java/RhumMono.java
@@ -1,6 +1,16 @@
-public class RhumMono extends Monocoque{
+/**
+ * La classe RhumMono représente un type spécifique de monocoque.
+ * Elle hérite de la classe Monocoque et permet de créer un bateau
+ * participant à la catégorie Rhum Mono avec un nom et un skipper.
+ */
+public class RhumMono extends Monocoque {
 
-
+    /**
+     * Constructeur qui crée un bateau RhumMono avec un nom et un skipper.
+     *
+     * @param nom     Le nom du bateau.
+     * @param skipper Le nom du skipper.
+     */
     public RhumMono(String nom, String skipper) {
         super(nom, skipper);
     }
