51784a6b473b010bf4c833f4aec8ebb150fe21ed
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-02 15:05:38 +0100

    "Ajout de commentaires pour la classe RouteOrthodromie.java

--- src/main/java/RouteOrthodromie.java
+++ src/main/java/RouteOrthodromie.java
@@ -1,5 +1,14 @@
+/**
+ * La classe RouteOrthodromie représente une route maritime empruntant l'orthodromie.
+ * Elle implémente l'interface SuivreRoute et définit la méthode suivreRoute
+ * pour afficher un message indiquant que cette route est suivie.
+ */
 public class RouteOrthodromie implements SuivreRoute {
-    public void suivreRoute(){
+
+    /**
+     * Suit la route orthodromique en affichant un message.
+     */
+    public void suivreRoute() {
         System.out.println("Vous avez emprunté la route orthodromie");
     }
 }
