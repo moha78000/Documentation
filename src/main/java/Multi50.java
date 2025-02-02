aad007c719e81569fa4b903670ced26936edb00b
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-02 14:58:34 +0100

    "Ajout de commentaires pour la classe Multi50.java

--- src/main/java/Multi50.java
+++ src/main/java/Multi50.java
@@ -1,9 +1,27 @@
+/**
+ * La classe Multi50 représente un type de multicoque spécifique.
+ * Elle hérite de la classe Multicoque et permet de créer un bateau
+ * de type Multi50 avec un nom et un skipper.
+ *
+ */
 public class Multi50 extends Multicoque {
 
+    /**
+     * Constructeur qui crée un bateau Multi50 avec un nom et un skipper.
+     *
+     * @param nom     Le nom du bateau.
+     * @param skipper Le nom du skipper.
+     */
     public Multi50(String nom, String skipper) {
         super(nom, skipper);
     }
 
+    /**
+     * Retourne une représentation textuelle de l'objet Multi50.
+     *
+     * @return Une chaîne de caractères contenant "Multi50" suivi des informations du multicoque.
+     */
+    @Override
     public String toString() {
         return "Multi50" + super.toString();
     }
